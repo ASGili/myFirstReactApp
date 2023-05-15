d7e5c8852dde28ecdbdb2b40fe44cf7ecfc20fcb
Aric Gilinsky <aric.gilinsky@gmail.com>
2023-05-15 18:55:36 +0100

    edit made

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name1 = "bruh"
+  let age1 = 23
   return (
+    <>
     <h1>Hello World</h1> 
+    <p> My name is {name1} and I'm {age1} years old</p>
+    </>
   );
 }
 
